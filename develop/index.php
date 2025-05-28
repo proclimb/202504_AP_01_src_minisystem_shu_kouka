40dcd793d6ef666d7cd0038151b686e1ddce9511
shu <202504ap01_shu@x-ide.sakura.ne.jp>
2025-05-28 11:54:07 +0900

    index.php

---
+++ develop/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>mini System</title>
+    <link rel="stylesheet" href="style_new.css">
+</head>
+
+<body>
+    <div>
+        <h1>mini System</h1>
+    </div>
+    <div>
+        <h2>TopPage</h2>
+    </div>
+    <div>
+        <form action="input.php" method="post" name="form">
+            <button type="submit">登録画面</button>
+        </form>
+        <form action="dashboard.php" method="post" name="form">
+            <button type="submit">ダッシュボード</button>
+        </form>
+    </div>
+</body>
+
+</html>
