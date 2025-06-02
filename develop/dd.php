d9f112b85e6f6aaba9f010e112141dc38dcf47a6
shu <202504ap01_shu@x-ide.sakura.ne.jp>
2025-06-02 10:53:35 +0900

    dd.phpを完了しました。

---
+++ develop/dd.php
@@ -0,0 +1,19 @@
+<?php
+$host = 'localhost';
+$dbname = 'minisystem';
+$user = 'root';
+$pass = 'proclimb';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+    die("DB接続に失敗しました： " . $e->getMessage());
+}
