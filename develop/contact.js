4c96e778f29a6cd420593afa0b55c835014a0218
shu <202504ap01_shu@x-ide.sakura.ne.jp>
2025-06-03 15:15:34 +0900

    JavaScript入力チェック（contact.js）が完了しました。

---
+++ develop/contact.js
@@ -0,0 +1,91 @@
+function validate() {
+    var flag = true;
+    removeElementsByClass("error");
+    removeClass("error-form");
+    if (document.edit.name.value == "") {
+        errorElement(document.edit.name, "お名前が入力されていません");
+        flag = false;
+    }
+    if (document.edit.name.value == "") {
+        errorElement(document.edit.kana, "ふりがなが入力されていません");
+        flag = false;
+    } else {
+        if (!validateKana(document.edit.kana.value)) {
+            errorElement(document.edit.kana, "ひらがなを入れて下さい");
+            flag = false;
+        }
+    }
+
+    if (document.edit.email.value == "") {
+        errorElement(document.edit.email, "メールアドレスが入力されていません");
+        flag = false;
+    } else {
+        if (!validateMail(document.edit.email.value)) {
+            errorElement(document.edit.email, "メールアドレスが正しくありません");
+            flag = false;
+        }
+    }
+
+    if (document.edit.tel.value == "") {
+        errorElement(document.edit.tel, "電話番号が入力されていません");
+        flag = false;
+    } else {
+        if (!validateTel(document.edit.tel.value)) {
+            errorElement(document.edit.tel, "電話番号が違います");
+            flag = false;
+        }
+    }
+
+    if (flag) {
+        document.edit.submit();
+    }
+
+    return false;
+}
+
+var errorElement = function (form, msg) {
+    form.className = "error-form";
+    var newElement = document.createElement("div");
+    newElement.className = "error";
+    var newText = document.createTextNode(msg);
+    newElement.appendChild(newText);
+    form.parentNode.insertBefore(newElement, form.nextSibling);
+}
+
+var removeElementsByClass = function (className) {
+    var elements = document.getElementsByClassName(className);
+    while (elements.length > 0) {
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+var removeClass = function (className) {
+    var elements = document.getElementsByClassName(className);
+    while (elements.length > 0) {
+        elements[0].className = "";
+    }
+}
+
+var validateMail = function (val) {
+    if (val.match(/^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@[A-Za-z0-9_.-]+.[A-Za-z0-9]+$/) == null) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+var validateTel = function (val) {
+    if (val.match(/^[0-9]{2,4}-[0-9]{2,4}-[0-9]{3,4}$/) == null) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+var validateKana = function (val) {
+    if (val.match(/^[ぁ-んー]+$/) == null) {
+        return false;
+    } else {
+        return true;
+    }
+}
